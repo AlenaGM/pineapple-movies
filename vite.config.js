5d318bd730b7f082717c0e8ff637e8a4f7158a3b
AlenaGM <alena.guillaume4@gmail.com>
2022-11-10 17:54:24 +0100

    upd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "pineapple-movies",
+  base: "/pineapple-movies/",
   plugins: [vue()],
 });
