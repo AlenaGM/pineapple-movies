f1be4953eb7088357eaa43cd2a78f7fb3ac8cd02
AlenaGM <alena.guillaume4@gmail.com>
2022-11-11 18:58:22 +0100

    update build for gh-pages

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -10,7 +10,7 @@ export const useMovieStore = defineStore("movieStore", {
           "After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
         poster_path: "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
         release_date: "2002-05-01",
-        isWatched: true,
+        isWatched: false,
       },
       {
         id: 2,
@@ -19,7 +19,7 @@ export const useMovieStore = defineStore("movieStore", {
           "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
         poster_path: "/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg",
         release_date: "2022-03-01",
-        isWatched: false,
+        isWatched: true,
       },
     ],
     activeTab: 1,
@@ -30,6 +30,18 @@ export const useMovieStore = defineStore("movieStore", {
     },
     totalCountMovies() {
       return this.movies.length;
+    },
+  },
+  actions: {
+    setActiveTab(id) {
+      this.activeTab = id;
+    },
+    toggleWatched(id) {
+      const idx = this.movies.findIndex((el) => el.id === id);
+      this.movies[idx].isWatched = !this.movies[idx].isWatched;
+    },
+    deleteMovie(id) {
+      this.movies = this.movies.filter((el) => el.id !== id);
     },
   },
 });
