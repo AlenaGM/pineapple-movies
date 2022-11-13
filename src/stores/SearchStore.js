1a23c6431dc3bdfc49316351cd3432b81b0d2340
AlenaGM <alena.guillaume4@gmail.com>
2022-11-13 14:51:38 +0100

    added async actions

---
+++ src/stores/SearchStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+
+const url =
+  "https:api.themoviedb.org/3/search/movie?api_key=6ad36d7cfa729ba7d5778bb108c77ec9&query=";
+
+export const useSearchStore = defineStore("searchStore", {
+  state: () => ({
+    loader: false,
+    movies: [],
+  }),
+  actions: {
+    async getMovies(search) {
+      this.loader = true;
+      const res = await fetch(`${url}${search}`);
+      const data = await res.json();
+      this.movies = data.results;
+      this.loader = false;
+    },
+  },
+});
